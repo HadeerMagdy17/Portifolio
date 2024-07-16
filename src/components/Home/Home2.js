9c3da748f926d716f23a424bbe6fe13b46b7b992
Hadeer Magdy <dody.magdy711@gmail.com>
2024-07-16 18:41:54 +0300

    remove un used code

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -4,8 +4,6 @@ import myImg from "../../Assets/intro.svg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
